cfdda7979f7f5dc8e0166f4403947805e0ada3f4
lempel <riverlempel@hotmail.com>
2020-01-06 18:29:00 +0800

    2020 init

---
+++ resource/about/about.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+    function showHobby(){
+		document.getElementById("content_other").innerHTML="<table id='tb'><tr><th>闲时踢足球</th></tr><tr><th>每周游游泳</th></tr><tr><th>偶尔会文艺</th></tr><tr><th>涂鸦是乐趣</th></tr></table><img id='forfun' alt='游戏' src='../resource/about/forfun.gif'/> ";
+	}
+	function showMsg(){
+		document.getElementById("content_other").innerHTML="<table id='tb'>                <tr><th>Name:</th><th>Lempel</th></tr><tr><th>Likes:</th><th><a id='panda_gungun'>Panda GunGun</th></tr> <tr><th>Major:</th><th>Computer Science</th></tr></table>";
+	}
+	function showMir(){
+		document.getElementById("content_other").innerHTML="<table id='tb'><tr><th>Don't put off till tomorrow </th><tr><tr><th>what should be done today.</th></tr><tr><th>今日事,今日毕</th></tr></table>";
+	}
+	
